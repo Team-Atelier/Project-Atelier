a16f13fb645a708bbdedb7ef4b1caa6a3890c9e4
maysieo <maysieocera@gmail.com>
2024-02-27 20:05:47 -0500

    Creating MORE conflict!

--- hello.js
+++ hello.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 console.log('Hi team ChickFilAsync create conflict');
-=======
 console.log('Hi team Chick-Fil-Async');
->>>>>>> bfa23d1b9de710c2c51a5357158e0855e2fff6cc
+
+console.log('What does this do?')
 
