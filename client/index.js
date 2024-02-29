9fe46c9888bd77a3042803bd7a0c5f473c162bd5
maysieo <maysieocera@gmail.com>
2024-02-28 19:21:02 -0500

    Built a server, mounted the react App component to webpage, added gitignore and babelrc, updated package.json

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+console.log("Test")
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const morgan = require('morgan');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at: http://localhost:${PORT}`);
+});
