16fa1a33f56a46e93782afb7ad18795c6300d4a6
maysieo <maysieocera@gmail.com>
2024-02-29 12:51:19 -0500

    Updated main app component with individual product page components

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import ProductDetail from './ProductDetail.jsx';
+import Reviews from './Reviews.jsx';
+import RelatedItems from './RelatedItems.jsx';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello, world</h1>
+      <h1>Project Atelier</h1>
+    </div>
+    <div>
+      <ProductDetail />
+    </div>
+    <div>
+      <Reviews />
+    </div>
+    <div>
+      <RelatedItems />
     </div>
   )
 }
