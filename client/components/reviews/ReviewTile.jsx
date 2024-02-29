5404bf39fca1251f86770eb778208ca5ebcfc906
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-02-29 15:36:42 -0800

    Added props to ReviewTile such that infomation is dynamically updatted based on whats been passed.

--- client/components/reviews/ReviewTile.jsx
+++ client/components/reviews/ReviewTile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import StarRating from './starRating.jsx'
+import { format, compareAsc } from "date-fns";
 
 const Title = styled.h1`
   margin: 0px;
@@ -19,49 +20,66 @@ const FlexRow = styled.section`
 const MainBox = styled.section`
   padding-left: 0em;
   padding-right: 0em;
-  margin-left: 20em;
-  margin-right: 20em;
+  margin-left: 0em;
+  margin-right: 0em;
   background: papayawhip;
   border-bottom: .25rem solid
 `;
 
 const ResponseBox = styled.section`
-  display: inline-block;
-  justify-content: space-between;
-  padding-left: 0em;
-  padding-right: 0em;
-  margin-left: 5em;
-  margin-right: 5em;
-  padding-left: 0%;
-  padding-right: 70%;
+  display: grid;
+  padding-right: 80%;
+  max-width: 150em;
   background: grey !important;
 `;
 
 const Right = styled.section`
 display: inline-block;
 `
+/*
+    let review = {
+      "review_id": 5,
+      "rating": 3,
+      "summary": "I'm enjoying wearing these shades",
+      "recommend": false,
+      "response": null,
+      "body": "Comfortable and practical.",
+      "date": "2019-04-14T00:00:00.000Z",
+      "reviewer_name": "shortandsweeet",
+      "helpfulness": 5,
+      "photos": [{
+          "id": 1,
+          "url": "urlplaceholder/review_5_photo_number_1.jpg"
+        },
+        {
+          "id": 2,
+          "url": "urlplaceholder/review_5_photo_number_2.jpg"
+        },
+        // ...
+      ]
+    }c
+*/
 
-const ReviewTile = ({})=>{
 
+
+const ReviewTile = ({review})=>{
+  const date = new Date(review.date);
   return (
     <article className = "reviewTile">
       <MainBox>
       <FlexRow>
-      <div className = "left"><StarRating/></div>
-      <Right><aside className = "username">{`[Username]`} 04 March 2018</aside></Right>
+      <div className = "left"><StarRating rating = {review.rating}/></div>
+      <Right><aside>{review.reviewer_name} (Verified Purchaser) {format(date, 'MMMM dd, y')}</aside></Right>
       </FlexRow>
       <FlexRow><Title>Review title</Title></FlexRow>
-      <FlexRow>Review summary</FlexRow>
-      <FlexRow>...summary continued (if nessessary)</FlexRow>
+      <FlexRow><b>{review.summary}</b></FlexRow>
       <br/>
-      <FlexRow>Review body</FlexRow>
+      <FlexRow>{review.body}</FlexRow>
+      {review.recommend && <><br/>
+      <FlexRow>✓ I recommend this product</FlexRow></>}
       <br/>
-      <FlexRow>✓ I recommend this product</FlexRow>
-      <br/>
-      <br/>
-      <FlexRow><ResponseBox>Response: <br/>
-      Response here</ResponseBox></FlexRow>
-      <br/>
+      {review.response && <><FlexRow><ResponseBox>Response: <br/> {review.response}</ResponseBox></FlexRow>
+      <br/></>}
       <FlexRow>Was this review helpful? (Yes 10) | Report</FlexRow>
 
 
