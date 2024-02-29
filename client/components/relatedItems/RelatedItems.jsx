bd0ba8f6ebdf515b900b3877e8a33c6bf8f49824
maysieo <maysieocera@gmail.com>
2024-02-29 13:19:19 -0500

    Created RelatedItems main component, with RelatedProductsList and YourOutfitList components linked--to be created.

---
+++ client/components/relatedItems/RelatedItems.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RelatedItems = () => {
+
+  return (
+    <div className="relatedProductsList">
+      <RelatedProductsList />
+    </div>
+    <div className="yourOutfitList">
+      <YourOutfitList />
+    </div>
+  )
+
+}
+
+export default RelatedItems;
