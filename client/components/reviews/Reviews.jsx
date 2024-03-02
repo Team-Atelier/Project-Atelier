aa591739e96fc54834f092b91c1b52e68db1aa17
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-03-01 16:22:34 -0800

    Updated review list to add sorting and to load more reviews when button pressed

--- client/components/reviews/Reviews.jsx
+++ client/components/reviews/Reviews.jsx
@@ -1,34 +1,90 @@
-import React, {useState, useEffect} from 'react'
-import ReviewTile from './ReviewTile.jsx'
-const Reviews = ()=>{
+/* eslint-disable no-console */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import ReviewTile from './ReviewTile.jsx';
 
+const url = process.env.API_URL;
+const token = process.env.GITHUB_TOKEN;
 
+// Check metadata to get full number of reviews.
+// Do the get requests once at the beginning and get all the reviews.
 
-    let review = {
-      "review_id": 5,
-      "rating": 3,
-      "summary": "I'm enjoying wearing these shades",
-      "recommend": true,
-      "response": "We are glad that you like our product!",
-      "body": "Comfortable and practical.",
-      "date": "2019-04-14T00:00:00.000Z",
-      "reviewer_name": "shortandsweeet",
-      "helpfulness": 5,
-      "photos": [{
-          "id": 1,
-          "url": "urlplaceholder/review_5_photo_number_1.jpg"
-        },
-        {
-          "id": 2,
-          "url": "urlplaceholder/review_5_photo_number_2.jpg"
-        },
-        // ...
-      ]
-    }
+function Reviews() {
+  const [relevantReviews, setRelevantReviews] = useState([]);
+  const [newestReviews, setNewestReviews] = useState([]);
+  const [helpfulReviews, setHelpfulReviews] = useState([]);
+  const [currentSort, setCurrentSort] = useState('relevant');
+  const [visibleReviews, setVisibleReviews] = useState(2);
 
-  return <ReviewTile review = {review}/>
+  const getNumberOfReviews = async () => {
+    const data = await axios.get(`${url}reviews/meta`, {
+      headers: { Authorization: token },
+      params: {
+        product_id: 40348,
+      },
+    });
+    let result = Object.values(data.data.ratings);
+    result = Object.values(data.data.ratings).map((item) => Number(item));
+    const sum = result.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return sum;
+  };
 
+  const getReviews = (count = 100, sort = 'relevant') => axios.get(`${url}reviews`, {
+    headers: { Authorization: token },
+    params: {
+      product_id: 40348,
+      count,
+      sort,
+    },
+  });
 
+  useEffect(() => {
+    getNumberOfReviews()
+      .then((numReviews) => Promise.all([getReviews(numReviews, 'relevant'), getReviews(numReviews, 'helpful'), getReviews(numReviews, 'newest')]))
+      .then((data) => {
+        const [relevant, helpful, newest] = data;
+        setRelevantReviews(relevant.data.results);
+        setNewestReviews(newest.data.results);
+        setHelpfulReviews(helpful.data.results);
+      });
+  }, []);
 
+  console.log(process.env.API_URL);
+  console.log(token);
+  const setSort = (e) => {
+    setVisibleReviews(2);
+    const method = e.target.value;
+    setCurrentSort(method);
+  };
+  const loadMoreReviews = () => {
+    setVisibleReviews(visibleReviews + 2);
+  };
+  return (
+    <>
+      <div>
+        Sort by:
+        {'  '}
+        <select value={currentSort} onChange={(e) => { setSort(e); }}>
+          <option value="relevant">Relevant</option>
+          <option value="helpful">Helpful</option>
+          <option value="newest">Newest</option>
+        </select>
+      </div>
+      {currentSort === 'relevant'
+      && relevantReviews
+        .toSpliced(visibleReviews, relevantReviews.length)
+        .map((item) => <ReviewTile key={item.review_id} review={item} />)}
+      {currentSort === 'helpful'
+      && helpfulReviews
+        .toSpliced(visibleReviews, relevantReviews.length)
+        .map((item) => <ReviewTile key={item.review_id} review={item} />)}
+      {currentSort === 'newest'
+      && newestReviews
+        .toSpliced(visibleReviews, relevantReviews.length)
+        .map((item) => <ReviewTile key={item.review_id} review={item} />)}
+      <button type="button" onClick={(e) => { loadMoreReviews(e); }}>More reviews</button>
+    </>
+  );
 }
 export default Reviews;
