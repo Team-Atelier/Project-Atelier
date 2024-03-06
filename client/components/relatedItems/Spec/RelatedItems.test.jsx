1b576c2e2f297f9815e9dafa8c9a9b3a6c74d17a
maysieo <maysieocera@gmail.com>
2024-03-05 21:03:48 -0500

    We got two tests going, ayyyyyy!

--- client/components/relatedItems/Spec/RelatedItems.test.jsx
+++ client/components/relatedItems/Spec/RelatedItems.test.jsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import RelatedItems from '../RelatedItems.jsx';
 
 test('renders RelatedProductsList component', () => {
-  render(<RelatedItems />);
+  const { getByText } = render(<RelatedItems />);
 
   expect(getByText('You might like...')).toBeInTheDocument();
+});
+
+test('renders YourOutfitList component', () => {
+  const { getByText } = render(<RelatedItems />);
+
+  expect(getByText('Build an ensemble')).toBeInTheDocument();
 });
