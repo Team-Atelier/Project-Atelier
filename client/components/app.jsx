8e02b10661e9d7402844e1e21f30614a4e3f4958
William Tucker <william.tucker29@gmail.com>
2024-03-08 10:55:20 -0600

    Wrapped Reviews in a div with the id "reviews-section". Wrote a function called scrollToReviews to scroll to the reviews component when a user clicks the link in InfoSection.jsx. Disabled linter error for import extensions.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,17 +1,27 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ProductDetail from './productDetail/ProductDetail.jsx';
 import Reviews from './reviews/Reviews.jsx';
 import RelatedItems from './relatedItems/RelatedItems.jsx';
 
 function App() {
+  const scrollToReviews = () => {
+    const reviewsSection = document.getElementById('reviews-section');
+    if (reviewsSection) {
+      reviewsSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
       <div>
         <h1>Project Atelier</h1>
       </div>
-        <ProductDetail />
-        <RelatedItems />
+      <ProductDetail scrollToReviews={scrollToReviews} />
+      <RelatedItems />
+      <div id="reviews-section">
         <Reviews />
+      </div>
     </>
   );
 }
