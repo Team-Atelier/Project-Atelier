c0cc1c53bbfda21539b86e6d5c297b5978915707
William Tucker <william.tucker29@gmail.com>
2024-03-12 13:49:23 -0500

    Removed console log statement and satisfied the linter gods.

--- client/index.js
+++ client/index.js
@@ -3,4 +3,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
